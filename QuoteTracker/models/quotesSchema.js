b7ffc303099a8e182388f779c33580ac7630174b
Jia790 <chenj2@oregonstate.edu>
2018-02-12 17:16:08 -0800

    Update quotesSchema.js
    functions of create, update, and delete has been moved to this.

--- QuoteTracker/models/quotesSchema.js
+++ QuoteTracker/models/quotesSchema.js
@@ -6,12 +6,12 @@ const QuoteSchema = new Schema({
 
     name : {
         type: String,
-        required: [true, 'Name field is required']  
+        required: [true, 'Name field is required']
     },
 
     quote : {
         type: String,
-        required: [true, 'Quote field is required']
+        required: [true, 'Name quote is required']
     }
 
 });
@@ -25,3 +25,36 @@ const Quote = mongoose.model('quote', QuoteSchema);
 
 // Export to use on other file
 module.exports = Quote;
+
+module.exports.addQuote = function(newQuote, res){
+    newQuote.save(function(err, doc){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.redirect('/');
+        }
+     }
+    
+    ); 
+}
+
+module.exports.updateTarget = function(req, res){
+    Quote.findOneAndUpdate({name: req.body.Ename}, { 
+        $set: { name: req.body.name, quote: req.body.quote} }, 
+          {sort: {_id: -1},upsert: false }, 
+            function(err, result){ if (err){ return res.send(err);} 
+               }).then(function(){ res.send(result); }).catch(next);
+}
+
+module.exports.deleteTarget = function(req, res){
+    Quote.findOneAndRemove({name: req.body.name},
+        function(err, result){
+          if (err) {return res.send(500, err);}
+          console.log('deleted from database');
+          //res.redirect('/')
+        }).then(function(){
+          res.send(result);
+        }
+        ).catch(next);
+}
